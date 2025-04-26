34bfda43d60d16976fdb9b56f779593817cfa54d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2025-04-26 17:25:07 -0600

    feat: globally enable the validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -5,7 +6,14 @@ import { AppModule } from './app.module';
 async function setup() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app
+    .useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+      }),
+    )
+    .enableCors();
 
   return app;
 }
