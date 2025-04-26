81cade7cf85c8fb22ae31fe2fa542eebff94e78b
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2025-04-26 13:30:13 -0600

    build: setup path aliases

--- test/app-e2e.spec.ts
+++ test/app-e2e.spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
+import type { App } from 'supertest/types';
 
-import { AppModule } from '../src/app.module';
+import { AppModule } from '~/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
--- vite.config.mts
+++ vite.config.mts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import { VitePluginNode } from 'vite-plugin-node';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     ...VitePluginNode({
       adapter: 'nest',
